4b7cdc665454885d3bdb4305ac50593c2db88b2b
Christian Jensen <christian.jensen@m.co>
2022-02-15 09:35:54 +0100

    Remove log persistence code and bump JDK to 11

--- log-persistence/src/main/java/co/m/common/utils/logging/persitance/PersistedLogLine.kt
+++
@@ -1,6 +0,0 @@
-package co.m.common.utils.logging.persitance
-
-import co.m.common.utils.logging.Line
-import co.m.common.utils.logging.Meta
-
-data class PersistedLogLine(val line: Line, val meta: Meta)
